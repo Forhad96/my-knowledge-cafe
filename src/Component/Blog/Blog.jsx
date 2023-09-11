15f315877f49bb3560e4494796c124d3a7eef039
Forhad96 <forhadairdrop@gmail.com>
2023-09-11 20:14:59 +0600

    basic setup and all components file created

---
+++ src/Component/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>blogs</h1>
+            
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/Component/Blogs/Blogs.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Blog from "../Blog/Blog";
+
+
+const Blogs = () => {
+    const [blogs,setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch('../../../public/blogs.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
+    return (
+        <div>
+            <Blog></Blog>
+        </div>
+    );
+};
+
+export default Blogs;
